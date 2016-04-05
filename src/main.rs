9c91a79f493ad90a6c62850431650e44e8d40da9
max mccarthy <maxmcc@seas.upenn.edu>
2016-04-05 19:02:46 -0400

    Testing that documentation generation is working

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #![doc(html_root_url = "https://maxmcc.github.io/rust-jvm/")]
 
+//! This is a comment.
+
+/// This function prints `"Hello, world!"` to the console.
 fn main() {
     println!("Hello, world!");
 }
