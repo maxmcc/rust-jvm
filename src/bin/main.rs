f2548951b5ed53f572a82c026252b3efc26a5394
max mccarthy <maxmcc@seas.upenn.edu>
2016-04-06 18:05:20 -0400

    Fix misplaced doc comment

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,7 +1,5 @@
 extern crate rust_jvm;
 
-//! A binary executable, mostly for testing doc generation.
-
 /// Prints a hello, world message to the console.
 fn main() {
     println!("Hello, world!");
