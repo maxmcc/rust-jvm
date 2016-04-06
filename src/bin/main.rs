286caf1e9b981381aef9e6ed06dac8e22152081a
max mccarthy <maxmcc@seas.upenn.edu>
2016-04-06 18:03:05 -0400

    Include bin/ directory for main exectuable

---
+++ src/bin/main.rs
@@ -0,0 +1,8 @@
+extern crate rust_jvm;
+
+//! A binary executable, mostly for testing doc generation.
+
+/// Prints a hello, world message to the console.
+fn main() {
+    println!("Hello, world!");
+}
