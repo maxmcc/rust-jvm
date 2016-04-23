3424fbb9fe04274c13d4713d83ba88555aec4756
Max McCarthy <maxmcc@seas.upenn.edu>
2016-04-23 00:07:39 -0400

    Display output of parsed class file

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,8 +1,15 @@
 extern crate rust_jvm;
 
-/// Prints a hello, world message to the console.
+use std::io::Read;
+
+use rust_jvm::parser::class_file;
+
 fn main() {
-    println!("Hello, world!");
-
-
+    let file_name = std::env::args().nth(1).unwrap();
+    let mut file = std::fs::File::open(file_name).unwrap();
+    let mut bytes = vec![];
+    file.read_to_end(&mut bytes).unwrap();
+    let class = class_file::parse_class_file(&bytes).unwrap();
+    println!("{:#?}", class);
 }
+
