fe127633f7a1ae65e47f33cb79e9e6aa7d0322fe
Max McCarthy <maxmcc@seas.upenn.edu>
2016-04-28 15:36:06 -0400

    Updated docs for StackFrame

--- src/vm/stack.rs
+++ src/vm/stack.rs
@@ -6,9 +6,12 @@ use vm::constant_pool::RuntimeConstantPool;
 #[derive(Debug)]
 pub struct Frame<'a> {
     /// The local variables of the current method.
+    /// Values that occupy two indices (`long` and `double`) are stored in one slot followed by a
+    /// `None` value in the subsequent index.
     local_variables: Vec<Option<Value>>,
     /// The operand stack manipulated by the instructions of the current method.
     operand_stack: Vec<Value>,
     /// A reference to the runtime constant pool of the current method's class.
     runtime_constant_pool: &'a RuntimeConstantPool,
 }
+
