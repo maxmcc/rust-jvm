560198c1db69ad0bce803395d842ff89c2dfaad8
Max McCarthy <maxmcc@seas.upenn.edu>
2016-04-29 02:12:33 -0400

    Remove attempt at "semantic bytecode" in favor of u8 representation. Move opcode module back into bytecode.rs.

--- src/vm/bytecode.rs
+++ src/vm/bytecode.rs
@@ -1,196 +1,207 @@
-/// An bytecode instruction in the Java virtual machine.
-#[derive(Debug)]
-pub enum Instruction {
-    Constant(op::Constant),
-    Load(op::Load),
-    Store(op::Store),
-    Stack(op::Stack),
-    Math(op::Math),
-    Conversion(op::Conversion),
-    Comparison(op::Comparison),
-    Control(op::Control),
-    Reference(op::Reference),
-    Extended(op::Extended),
+mod opcode {
+    pub const NOP: u8 = 0x00;
+    pub const ACONST_NULL: u8 = 0x01;
+    pub const ICONST_M1: u8 = 0x02;
+    pub const ICONST_0: u8 = 0x03;
+    pub const ICONST_1: u8 = 0x04;
+    pub const ICONST_2: u8 = 0x05;
+    pub const ICONST_3: u8 = 0x06;
+    pub const ICONST_4: u8 = 0x07;
+    pub const ICONST_5: u8 = 0x08;
+    pub const LCONST_0: u8 = 0x09;
+    pub const LCONST_1: u8 = 0x0a;
+    pub const FCONST_0: u8 = 0x0b;
+    pub const FCONST_1: u8 = 0x0c;
+    pub const FCONST_2: u8 = 0x0d;
+    pub const DCONST_0: u8 = 0x0e;
+    pub const DCONST_1: u8 = 0x0f;
+    pub const BIPUSH: u8 = 0x10;
+    pub const SIPUSH: u8 = 0x11;
+    pub const LDC: u8 = 0x12;
+    pub const LDC_W: u8 = 0x13;
+    pub const LDC2_W: u8 = 0x14;
+    pub const ILOAD: u8 = 0x15;
+    pub const LLOAD: u8 = 0x16;
+    pub const FLOAD: u8 = 0x17;
+    pub const DLOAD: u8 = 0x18;
+    pub const ALOAD: u8 = 0x19;
+    pub const ILOAD_0: u8 = 0x1a;
+    pub const ILOAD_1: u8 = 0x1b;
+    pub const ILOAD_2: u8 = 0x1c;
+    pub const ILOAD_3: u8 = 0x1d;
+    pub const LLOAD_0: u8 = 0x1e;
+    pub const LLOAD_1: u8 = 0x1f;
+    pub const LLOAD_2: u8 = 0x20;
+    pub const LLOAD_3: u8 = 0x21;
+    pub const FLOAD_0: u8 = 0x22;
+    pub const FLOAD_1: u8 = 0x23;
+    pub const FLOAD_2: u8 = 0x24;
+    pub const FLOAD_3: u8 = 0x25;
+    pub const DLOAD_0: u8 = 0x26;
+    pub const DLOAD_1: u8 = 0x27;
+    pub const DLOAD_2: u8 = 0x28;
+    pub const DLOAD_3: u8 = 0x29;
+    pub const ALOAD_0: u8 = 0x2a;
+    pub const ALOAD_1: u8 = 0x2b;
+    pub const ALOAD_2: u8 = 0x2c;
+    pub const ALOAD_3: u8 = 0x2d;
+    pub const IALOAD: u8 = 0x2e;
+    pub const LALOAD: u8 = 0x2f;
+    pub const FALOAD: u8 = 0x30;
+    pub const DALOAD: u8 = 0x31;
+    pub const AALOAD: u8 = 0x32;
+    pub const BALOAD: u8 = 0x33;
+    pub const CALOAD: u8 = 0x34;
+    pub const SALOAD: u8 = 0x35;
+    pub const ISTORE: u8 = 0x36;
+    pub const LSTORE: u8 = 0x37;
+    pub const FSTORE: u8 = 0x38;
+    pub const DSTORE: u8 = 0x39;
+    pub const ASTORE: u8 = 0x3a;
+    pub const ISTORE_0: u8 = 0x3b;
+    pub const ISTORE_1: u8 = 0x3c;
+    pub const ISTORE_2: u8 = 0x3d;
+    pub const ISTORE_3: u8 = 0x3e;
+    pub const LSTORE_0: u8 = 0x3f;
+    pub const LSTORE_1: u8 = 0x40;
+    pub const LSTORE_2: u8 = 0x41;
+    pub const LSTORE_3: u8 = 0x42;
+    pub const FSTORE_0: u8 = 0x43;
+    pub const FSTORE_1: u8 = 0x44;
+    pub const FSTORE_2: u8 = 0x45;
+    pub const FSTORE_3: u8 = 0x46;
+    pub const DSTORE_0: u8 = 0x47;
+    pub const DSTORE_1: u8 = 0x48;
+    pub const DSTORE_2: u8 = 0x49;
+    pub const DSTORE_3: u8 = 0x4a;
+    pub const ASTORE_0: u8 = 0x4b;
+    pub const ASTORE_1: u8 = 0x4c;
+    pub const ASTORE_2: u8 = 0x4d;
+    pub const ASTORE_3: u8 = 0x4e;
+    pub const IASTORE: u8 = 0x4f;
+    pub const LASTORE: u8 = 0x50;
+    pub const FASTORE: u8 = 0x51;
+    pub const DASTORE: u8 = 0x52;
+    pub const AASTORE: u8 = 0x53;
+    pub const BASTORE: u8 = 0x54;
+    pub const CASTORE: u8 = 0x55;
+    pub const SASTORE: u8 = 0x56;
+    pub const POP: u8 = 0x57;
+    pub const POP2: u8 = 0x58;
+    pub const DUP: u8 = 0x59;
+    pub const DUP_X1: u8 = 0x5a;
+    pub const DUP_X2: u8 = 0x5b;
+    pub const DUP2: u8 = 0x5c;
+    pub const DUP2_X1: u8 = 0x5d;
+    pub const DUP2_X2: u8 = 0x5e;
+    pub const SWAP: u8 = 0x5f;
+    pub const IADD: u8 = 0x60;
+    pub const LADD: u8 = 0x61;
+    pub const FADD: u8 = 0x62;
+    pub const DADD: u8 = 0x63;
+    pub const ISUB: u8 = 0x64;
+    pub const LSUB: u8 = 0x65;
+    pub const FSUB: u8 = 0x66;
+    pub const DSUB: u8 = 0x67;
+    pub const IMUL: u8 = 0x68;
+    pub const LMUL: u8 = 0x69;
+    pub const FMUL: u8 = 0x6a;
+    pub const DMUL: u8 = 0x6b;
+    pub const IDIV: u8 = 0x6c;
+    pub const LDIV: u8 = 0x6d;
+    pub const FDIV: u8 = 0x6e;
+    pub const DDIV: u8 = 0x6f;
+    pub const IREM: u8 = 0x70;
+    pub const LREM: u8 = 0x71;
+    pub const FREM: u8 = 0x72;
+    pub const DREM: u8 = 0x73;
+    pub const INEG: u8 = 0x74;
+    pub const LNEG: u8 = 0x75;
+    pub const FNEG: u8 = 0x76;
+    pub const DNEG: u8 = 0x77;
+    pub const ISHL: u8 = 0x78;
+    pub const LSHL: u8 = 0x79;
+    pub const ISHR: u8 = 0x7a;
+    pub const LSHR: u8 = 0x7b;
+    pub const IUSHR: u8 = 0x7c;
+    pub const LUSHR: u8 = 0x7d;
+    pub const IAND: u8 = 0x7e;
+    pub const LAND: u8 = 0x7f;
+    pub const IOR: u8 = 0x80;
+    pub const LOR: u8 = 0x81;
+    pub const IXOR: u8 = 0x82;
+    pub const LXOR: u8 = 0x83;
+    pub const IINC: u8 = 0x84;
+    pub const I2L: u8 = 0x85;
+    pub const I2F: u8 = 0x86;
+    pub const I2D: u8 = 0x87;
+    pub const L2I: u8 = 0x88;
+    pub const L2F: u8 = 0x89;
+    pub const L2D: u8 = 0x8a;
+    pub const F2I: u8 = 0x8b;
+    pub const F2L: u8 = 0x8c;
+    pub const F2D: u8 = 0x8d;
+    pub const D2I: u8 = 0x8e;
+    pub const D2L: u8 = 0x8f;
+    pub const D2F: u8 = 0x90;
+    pub const I2B: u8 = 0x91;
+    pub const I2C: u8 = 0x92;
+    pub const I2S: u8 = 0x93;
+    pub const LCMP: u8 = 0x94;
+    pub const FCMPL: u8 = 0x95;
+    pub const FCMPG: u8 = 0x96;
+    pub const DCMPL: u8 = 0x97;
+    pub const DCMPG: u8 = 0x98;
+    pub const IFEQ: u8 = 0x99;
+    pub const IFNE: u8 = 0x9a;
+    pub const IFLT: u8 = 0x9b;
+    pub const IFGE: u8 = 0x9c;
+    pub const IFGT: u8 = 0x9d;
+    pub const IFLE: u8 = 0x9e;
+    pub const IF_ICMPEQ: u8 = 0x9f;
+    pub const IF_ICMPNE: u8 = 0xa0;
+    pub const IF_ICMPLT: u8 = 0xa1;
+    pub const IF_ICMPGE: u8 = 0xa2;
+    pub const IF_ICMPGT: u8 = 0xa3;
+    pub const IF_ICMPLE: u8 = 0xa4;
+    pub const IF_ACMPEQ: u8 = 0xa5;
+    pub const IF_ACMPNE: u8 = 0xa6;
+    pub const GOTO: u8 = 0xa7;
+    pub const JSR: u8 = 0xa8;
+    pub const RET: u8 = 0xa9;
+    pub const TABLESWITCH: u8 = 0xaa;
+    pub const LOOKUPSWITCH: u8 = 0xab;
+    pub const IRETURN: u8 = 0xac;
+    pub const LRETURN: u8 = 0xad;
+    pub const FRETURN: u8 = 0xae;
+    pub const DRETURN: u8 = 0xaf;
+    pub const ARETURN: u8 = 0xb0;
+    pub const RETURN: u8 = 0xb1;
+    pub const GETSTATIC: u8 = 0xb2;
+    pub const PUTSTATIC: u8 = 0xb3;
+    pub const GETFIELD: u8 = 0xb4;
+    pub const PUTFIELD: u8 = 0xb5;
+    pub const INVOKEVIRTUAL: u8 = 0xb6;
+    pub const INVOKESPECIAL: u8 = 0xb7;
+    pub const INVOKESTATIC: u8 = 0xb8;
+    pub const INVOKEINTERFACE: u8 = 0xb9;
+    pub const INVOKEDYNAMIC: u8 = 0xba;
+    pub const NEW: u8 = 0xbb;
+    pub const NEWARRAY: u8 = 0xbc;
+    pub const ANEWARRAY: u8 = 0xbd;
+    pub const ARRAYLENGTH: u8 = 0xbe;
+    pub const ATHROW: u8 = 0xbf;
+    pub const CHECKCAST: u8 = 0xc0;
+    pub const INSTANCEOF: u8 = 0xc1;
+    pub const MONITORENTER: u8 = 0xc2;
+    pub const MONITOREXIT: u8 = 0xc3;
+    pub const WIDE: u8 = 0xc4;
+    pub const MULTIANEWARRAY: u8 = 0xc5;
+    pub const IFNULL: u8 = 0xc6;
+    pub const IFNONNULL: u8 = 0xc7;
+    pub const GOTO_W: u8 = 0xc8;
+    pub const JSR_W: u8 = 0xc9;
+    pub const BREAKPOINT: u8 = 0xca;
+    pub const IMPDEP1: u8 = 0xfe;
+    pub const IMPDEP2: u8 = 0xff;
 }
-
-mod op {
-    use model::class_file::constant_pool_index;
-
-    pub type LocalVariableIndex = u16;
-    pub type BranchOffset = i32;
-
-    /// Push a constant value onto the operand stack.
-    #[derive(Debug)]
-    #[allow(non_camel_case_types)]
-    pub enum Constant {
-        Aconst_null,
-        Ipush { value: i32 },
-        Lconst_0,
-        Lconst_1,
-        Fconst_0,
-        Fconst_1,
-        Fconst_2,
-        Dconst_0,
-        Dconst_1,
-        Ldc { index: constant_pool_index },
-    }
-
-    /// Load a value from a local variable or array onto the operand stack.
-    #[derive(Debug)]
-    #[allow(non_camel_case_types)]
-    pub enum Load {
-        Iload { index: LocalVariableIndex },
-        Lload { index: LocalVariableIndex },
-        Fload { index: LocalVariableIndex },
-        Dload { index: LocalVariableIndex },
-        Aload { index: LocalVariableIndex },
-        Iaload,
-        Laload,
-        Faload,
-        Daload,
-        Aaload,
-        Baload,
-        Caload,
-        Saload,
-    }
-
-    /// Store a value on the operand stack into a local variable or array.
-    #[derive(Debug)]
-    #[allow(non_camel_case_types)]
-    pub enum Store {
-        Istore { index: LocalVariableIndex },
-        Lstore { index: LocalVariableIndex },
-        Fstore { index: LocalVariableIndex },
-        Dstore { index: LocalVariableIndex },
-        Astore { index: LocalVariableIndex },
-        Iastore,
-        Lastore,
-        Fastore,
-        Dastore,
-        Aastore,
-        Bastore,
-        Castore,
-        Sastore,
-    }
-
-    /// Manipulate values on the operand stack.
-    #[derive(Debug)]
-    #[allow(non_camel_case_types)]
-    pub enum Stack {
-        Pop,
-        Pop2,
-        Dup,
-        Dup_x1,
-        Dup_x2,
-        Dup2,
-        Dup2_x1,
-        Dup2_x2,
-        Swap,
-    }
-
-    /// Perform mathematical operations on stack values.
-    #[derive(Debug)]
-    #[allow(non_camel_case_types)]
-    pub enum Math {
-        Add,
-        Sub,
-        Mul,
-        Div,
-        Rem,
-        Neg,
-        Shl,
-        Shr,
-        Ushr,
-        And,
-        Or,
-        Xor,
-        Iinc { index: LocalVariableIndex, constant: i16 },
-    }
-
-    /// Convert stack values between JVM types.
-    #[derive(Debug)]
-    pub enum Conversion {
-        I2l,
-        I2f,
-        I2d,
-        L2i,
-        L2f,
-        L2d,
-        F2i,
-        F2l,
-        F2d,
-        D2i,
-        D2l,
-        D2f,
-        I2b,
-        I2c,
-        I2s,
-    }
-
-    /// Compare stack values conditionally alter control flow.
-    #[derive(Debug)]
-    #[allow(non_camel_case_types)]
-    pub enum Comparison {
-        Lcmp,
-        Fcmpl,
-        Fcmpg,
-        Dcmpl,
-        Dcmpg,
-        Ifeq { offset: BranchOffset },
-        Ifne { offset: BranchOffset },
-        Iflt { offset: BranchOffset },
-        Ifge { offset: BranchOffset },
-        Ifgt { offset: BranchOffset },
-        Ifle { offset: BranchOffset },
-        If_icmpeq { offset: BranchOffset },
-        If_icmpne { offset: BranchOffset },
-        If_icmplt { offset: BranchOffset },
-        If_icmpge { offset: BranchOffset },
-        If_icmpgt { offset: BranchOffset },
-        If_icmple { offset: BranchOffset },
-        If_acmpeq { offset: BranchOffset },
-        If_acmpne { offset: BranchOffset },
-    }
-
-    /// Unconditionally modify control flow.
-    #[derive(Debug)]
-    pub enum Control {
-        Goto { offset: BranchOffset },
-        Tableswitch { bytes: Vec<u8> },
-        Lookupswitch { bytes: Vec<u8> },
-        ReturnValue,
-        ReturnVoid,
-    }
-
-    /// Operations on reference types (objects and arrays).
-    #[derive(Debug)]
-    pub enum Reference {
-        Getstatic { index: constant_pool_index },
-        Putstatic { index: constant_pool_index },
-        Getfield { index: constant_pool_index },
-        Putfield { index: constant_pool_index },
-        Invokevirtual { index: constant_pool_index },
-        Invokespecial { index: constant_pool_index },
-        Invokestatic { index: constant_pool_index },
-        New { index: constant_pool_index },
-        Newarray { element_type: u8 },
-        Anewarray { index: constant_pool_index },
-        Arraylength,
-        Athrow,
-        Checkcast { index: constant_pool_index },
-        Instanceof { index: constant_pool_index },
-        Monitorenter,
-        Monitorexit,
-    }
-
-    /// Extended instructions supported by the JVM.
-    #[derive(Debug)]
-    #[allow(non_camel_case_types)]
-    pub enum Extended {
-        Multianewarray { index: constant_pool_index, dimensions: u8 },
-        Ifnull { offset: BranchOffset },
-        Ifnonnull { offset: BranchOffset },
-        Goto_w,
-    }
-}
-
