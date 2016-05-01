448ab2f239bd499cfed87dea6537bfc71a3ff0b8
Max McCarthy <maxmcc@seas.upenn.edu>
2016-05-01 01:37:42 -0400

    Update vm/ module-level docs

--- src/vm/mod.rs
+++ src/vm/mod.rs
@@ -1,4 +1,4 @@
-//! Structures for linked classes.
+//! The public interface for the Java virtual machine.
 
 mod bytecode;
 mod class;
@@ -10,34 +10,45 @@ mod value;
 
 use self::class_loader::ClassLoader;
 
-/// References to unlinked structures from the runtime constant pool.
+/// A symbolic reference to an entity in the runtime constant pool (§5.1). Symbolic references
+/// must be resolved (§5.4.3) before their usage by the interpreter.
 pub mod symref {
     use vm::sig;
 
     #[derive(Debug, Clone, PartialEq, Eq, Hash)]
+    /// A symbolic reference to a class constant pool entry.
     pub struct Class {
+        /// The signature of the class to which the symbolic reference refers.
         pub sig: sig::Class,
     }
 
     #[derive(Debug, Clone, PartialEq, Eq, Hash)]
+    /// A symbolic reference to a field constant pool entry.
     pub struct Field {
+        /// A symbolic reference to the class containing this field.
         pub class: Class,
+        /// The signature of the field to which the symbolic reference refers.
         pub sig: sig::Field,
     }
 
     #[derive(Debug, Clone, PartialEq, Eq, Hash)]
+    /// A symbolic reference to a method constant pool entry.
     pub struct Method {
+        /// A symbolic reference to the class containing this method.
         pub class: Class,
+        /// The signature of the method to which the symbolic reference refers.
         pub sig: sig::Method,
     }
 }
 
-/// Descriptors for things in the runtime constant pool.
+/// Signatures of runtime constant pool entities that serve to uniquely identify those entities.
+/// These are derived from structures in the binary representation of the constant pool (§5.1).
 pub mod sig {
     use std::num::Wrapping;
     use vm::value::Value;
 
     #[derive(Debug, Clone, PartialEq, Eq, Hash)]
+    /// Java language type information.
     pub enum Type {
         Byte,
         Char,
@@ -60,6 +71,7 @@ pub mod sig {
             }
         }
 
+        /// Compute a sequence of types from a JVM-internal string representation.
         pub fn new_multi(multi_type_str: &str) -> (Vec<Self>, &str) {
             let mut types = vec![];
             let mut remainder = multi_type_str;
@@ -76,6 +88,7 @@ pub mod sig {
             (types, remainder)
         }
 
+        /// Compute a type from a JVM-internal string representation.
         fn new_partial(type_str: &str) -> Result<(Self, &str), ()> {
             let (specifier, rest) = type_str.split_at(1);
             match specifier {
@@ -103,6 +116,8 @@ pub mod sig {
             }
         }
 
+        /// Get the default value for the type. Java's default values for integers is `0`, floats
+        /// `0.0`, characters `'\0'`, booleans `false`, and references `null`.
         pub fn default_value(&self) -> Value {
             match *self {
                 Type::Byte | Type::Char | Type::Int | Type::Short | Type::Boolean =>
@@ -116,8 +131,11 @@ pub mod sig {
     }
 
     #[derive(Debug, Clone, PartialEq, Eq, Hash)]
+    /// A class signature.
     pub enum Class {
+        /// The signature of a non-array class, parametrized by its name.
         Scalar(String),
+        /// The signature of an array class, parametrized by the type of elements in the array.
         Array(Box<Type>),
     }
 
@@ -146,15 +164,22 @@ pub mod sig {
     }
 
     #[derive(Debug, Clone, PartialEq, Eq, Hash)]
+    /// A field signature.
     pub struct Field {
+        /// The name of the field.
         pub name: String,
+        /// The type of the field.
         pub ty: Type,
     }
 
     #[derive(Debug, Clone, PartialEq, Eq, Hash)]
+    /// A method signature.
     pub struct Method {
+        /// The name of the method.
         pub name: String,
+        /// The types of the parameters of the method.
         pub params: Vec<Type>,
+        /// The return type of the method, or `None` if the method is `void`.
         pub return_ty: Option<Type>,
     }
 
@@ -185,7 +210,15 @@ pub mod sig {
 }
 
 #[derive(Debug)]
+/// The top-level virtual machine. The virtual machine contains a reference to its _bootstrap class
+/// loader_, which is used to load the main class and all of that class's dependencies.
+///
+/// The virtual machine's `start` method causes initialization of the boostrap class loader, which
+/// loads the main class and invokes the `main(String[])` method to begin execution of the program.
+/// The virtual machine terminates either when the program completes successfully, or when there is
+/// an uncaught error in the program or the internal implementation of the virtual machine itself.
 pub struct VirtualMachine {
+    /// The bootstrap class loader used to initialize the virtual machine.
     bootstrap_class_loader: ClassLoader,
 }
 
@@ -196,6 +229,7 @@ impl VirtualMachine {
         }
     }
 
+    /// Begin execution of the virtual machine instance's `main(String[])` method.
     pub fn start(mut self, main_class: symref::Class) {
         let class = self.bootstrap_class_loader.load_class(&main_class.sig).unwrap();
         class.initialize(&mut self.bootstrap_class_loader);
